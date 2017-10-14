e159d22df01a678cf9388a114fc15bf60c2f94fb
William Bradley <tknomanzr@testbed.interstitialdesign.bounceme.net>
2017-10-14 15:07:44 -0500

    Implemented single instance handling for per_cpu.py and show_bitcoin_detail.py. I used to different implementations. One uses fcntl and one uses sockets. At present, if I use only one implementation, it makes it where only one details window can be open per tint2 session. This might be ok but what I really had in mi nd was only on script instance runs per executor.

--- tint2/executors/bitcoin/show_bitcoin_detail.py
+++ tint2/executors/bitcoin/show_bitcoin_detail.py
@@ -11,27 +11,27 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 plt.style.use('dark_background')
 import datetime
-import sys
-#The following ensures that only one instance of this program will run.
-try:
-    import socket
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    ## Create an abstract socket, by prefixing it with null. 
-    s.bind( '\0postconnect_gateway_notify_lock') 
-except socket.error as e:
-    error_code = e.args[0]
-    error_string = e.args[1]
-    print "Process already running (%d:%s ). Exiting" % ( error_code, error_string) 
-    sys.exit (0)
-# Get historical bitcoin price information from gdax     
-client = gdax.PublicClient()
-rates = client.get_product_historic_rates('BTC-USD', granularity=60*60*24)
-# Modify the dates from ISO 8601 to human readable.
-dates = [datetime.date.fromtimestamp(element[0]) for element in rates]
-# Create the pandas plot. 
-df = pd.DataFrame(rates, columns = ['date', 'low', 'high', 'open', 'close', 'volume'])
-df['date'] = [datetime.date.fromtimestamp(element[0]) for element in rates]
-df = df.head(30)
-df = df.sort_values('date', ascending = True)
-df.plot('date', 'close', kind='line', use_index=True, title='Bitcoin closing prices by date', rot=90, grid=True, figsize=[6,9])
-plt.show()
+import subprocess
+import os, fcntl
+def run_once():
+    global fh
+    fh=open(os.path.realpath(__file__),'r')
+    try:
+        fcntl.flock(fh,fcntl.LOCK_EX|fcntl.LOCK_NB)
+    except:
+        os._exit(0)
+if __name__ == "__main__":
+	run_once()
+	# Get historical bitcoin price information from gdax     
+	client = gdax.PublicClient()
+	rates = client.get_product_historic_rates('BTC-USD', granularity=60*60*24)
+	# Modify the dates from ISO 8601 to human readable.
+	dates = [datetime.date.fromtimestamp(element[0]) for element in rates]
+	# Create the pandas plot. 
+	df = pd.DataFrame(rates, columns = ['date', 'low', 'high', 'open', 'close', 'volume'])
+	df['date'] = [datetime.date.fromtimestamp(element[0]) for element in rates]
+	df = df.head(30)
+	df = df.sort_values('date', ascending = True)
+	df.plot('date', 'close', kind='line', use_index=True, title='Bitcoin closing prices by date', rot=90, grid=True, figsize=[6,9])
+	plt.show()
+	
